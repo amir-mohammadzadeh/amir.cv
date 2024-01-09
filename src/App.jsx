98ebce54b216275fc652ec7bf7e9fd3374f8b8c4
amir <amir.code98@gmail.com>
2024-01-09 13:08:12 +0330

    fix: mobile screen size

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
     return (
         <>
-            <div dir='rtl' className="h-screen w-screen py-8 px-6 flex gap-4 lg:flex-col-reverse lg:p-0 lg:pt-4">
+            <div dir='rtl' className="w-full h-full py-8 px-6 flex gap-4 lg:flex-col-reverse lg:p-0 lg:pt-4">
                 <Navigation theme={darkThem} setTheme={setDarkThem} />
                 <main className="main">
                     {routes}
